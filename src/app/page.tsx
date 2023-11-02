e59fb69d5bcb4b0038c05f465c211f7a201d78db
Tomas Kostadinov <github@kostadinov.it>
2023-11-02 18:58:05 +0100

    Implement initial version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,111 @@
-import Image from 'next/image'
+"use client";
+import {useEffect, useState} from "react";
+import {color as d3color} from "d3-color";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    // save state of colors
+    const [colors, setColors] = useState<Color[]>([]);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const generateHSL = (): HSLColor => {
+        // Generate a random hue value between 0 and 360
+        const hue = Math.floor(Math.random() * 360);
+        // Set constant saturation and lightness values for flat colors
+        const saturation = getRandomArbitrary(0, 100); // You can adjust this value
+        const lightness = getRandomArbitrary(10, 100); // You can adjust this value
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        // Convert HSL to a hexadecimal color code
+        return {
+            h: hue,
+            s: saturation,
+            l: lightness,
+            cssString: `hsl(${hue}, ${saturation}%, ${lightness}%)`,
+        };
+    }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    // listen to spacebar keypress
+    useEffect(() => {
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+        if (!d3color) return;
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        let tempColors: Color[] = [];
+
+        for (let i = 0; i < 5; i++) {
+            const randomColor = generateHSL();
+
+            tempColors = [...tempColors, {
+                hsl: randomColor,
+                hex: d3color(randomColor.cssString)?.formatHex() || '',
+                isLocked: false,
+            }];
+        }
+        setColors(tempColors);
+
+        const handleKeyPress = (event: KeyboardEvent) => {
+            if (event.key === ' ' || event.code === "Space") {
+                // Run your function here
+                regenerateColors();
+            }
+        };
+
+        const regenerateColors = () => {
+            let newColors: Color[] = [];
+            tempColors.forEach(color => {
+                if (color.isLocked) {
+                    newColors = [...newColors, color];
+                } else {
+                    const randomColor = generateHSL();
+                    newColors = [...newColors, {
+                        hsl: randomColor,
+                        hex: d3color(randomColor.cssString)?.formatHex() || '',
+                        isLocked: false,
+                    }];
+                }
+            });
+
+            setColors(newColors);
+        };
+
+        document.addEventListener('keydown', handleKeyPress);
+
+        return () => {
+            // Clean up the event listener when the component unmounts
+            document.removeEventListener('keydown', handleKeyPress);
+        };
+    }, []);
+
+
+    return (
+        <main className="flex h-screen">
+            {
+                colors.map((color, index) => (
+                    <div
+                        key={index}
+                        className="flex-1 flex flex-col justify-center items-center text-black"
+                        style={{backgroundColor: color.hex}}
+                    >
+                        <span className="text-3xl slashed-zero lining-nums proportional-nums font-mono uppercase font-bold leading-3">
+                            {color.hex}
+                        </span>
+                    </div>
+                ))
+            }
+        </main>
+    )
+}
+
+export interface Color {
+    hsl: HSLColor;
+    hex: string;
+    isLocked: boolean;
+}
+
+function getRandomArbitrary(min: number, max: number): number {
+    return Math.random() * (max - min) + min;
+}
+
+interface HSLColor {
+    h: number;
+    s: number;
+    l: number;
+    cssString: string;
 }
